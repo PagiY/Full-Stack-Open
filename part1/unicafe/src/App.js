7ffe3693297c6618060b82b99b42983a2d9b31b3
PagiY <pagiyap003@gmail.com>
2023-05-12 22:18:57 +0800

    Finished until Part 1.7 (Unicafe step 2)

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const Button = ({handleClick, title}) => (<button onClick = {handleClick}>{title}</button>)
+
+const ShowCounts = ({title, count}) => (
+  <>
+    <p>{title} {count}</p>
+  </>
+)
+
+const App = () => {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+  
+  const handleClick = (value, hook) => {
+    hook(value + 1);
+    const newGood = good;
+    const newBad = bad;
+    const newTotal = total + 1
+    const newAvg = (newGood - newBad) / newTotal;
+    const newPositive = newGood / newTotal;
+    setTotal(newTotal);
+    setAverage(newAvg);
+    setPositive(newPositive);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleClick={()=>{handleClick(good, setGood)}} title = "Good"/>
+      <Button handleClick={()=>{handleClick(neutral, setNeutral)}} title = "Neutral"/>
+      <Button handleClick={()=>{handleClick(bad, setBad)}} title = "Bad"/>
+      <h1>Statistics</h1>
+      <ShowCounts title = "good" count = {good}/>
+      <ShowCounts title = "neutral" count = {neutral}/>
+      <ShowCounts title = "bad" count = {bad}/>
+      <ShowCounts title = "all" count = {total}/>
+      <ShowCounts title = "average" count = {average}/>
+      <ShowCounts title = "positive" count = {`${positive}%`}/>
     </div>
   );
 }
