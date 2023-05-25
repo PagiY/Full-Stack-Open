8fa6f0ee5306fca737c7574d299dc9046be519b4
PagiY <pagiyap003@gmail.com>
2023-05-25 21:28:10 +0800

    Added middleware for serving static files

--- part3/index.js
+++ part3/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'));
+app.use(express.static('build'));
 
 morgan.token('type', function (req, res) { 
     return JSON.stringify(req.body) 
@@ -11,24 +12,29 @@ morgan.token('type', function (req, res) {
 
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+    },
+    { 
+        "id": 5,
+        "name": "Test", 
+        "number": "39-23-6423122"
     }
 ]
 
@@ -96,7 +102,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>{ 
     console.log(`Listening at PORT ${PORT}`);
 });
