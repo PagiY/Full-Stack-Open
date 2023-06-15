63a6e904f7c70aacb413554a22d9dbe649136a99
PagiY <pagiyap003@gmail.com>
2023-06-15 19:56:14 +0800

    Added eslint rules

---
+++ part3/phonebook/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'node': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': 0,
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': [
+			'error', 'always'
+		],
+		'arrow-spacing': [
+			'error', { 'before': true, 'after': true }
+		]
+	}
+};
