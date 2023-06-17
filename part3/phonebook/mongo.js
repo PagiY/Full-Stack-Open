801b53777e146cd3d0e64de2959e1ed0bbaed8a1
PagiY <pagiyap003@gmail.com>
2023-06-17 17:33:29 +0800

    fixed eslint errors

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 if(process.argv.length < 3){
-    console.log('Please provide the password as argument.');
-    process.exit(1);
+	console.log('Please provide the password as argument.');
+	process.exit(1);
 }
 
 const password = process.argv[2];
@@ -12,35 +12,35 @@ mongoose.set('strictQuery', false);
 mongoose.connect(url);
 
 const phonebookSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-})
+	name: String,
+	number: String,
+});
 
 const Phonebook = mongoose.model('phonebook', phonebookSchema);
 
 if(process.argv.length > 3){
-    const name = process.argv[3];
-    const number = process.argv[4];
+	const name = process.argv[3];
+	const number = process.argv[4];
 
-    const person = new Phonebook({
-        name: name,
-        number: number,
-    })
+	const person = new Phonebook({
+		name: name,
+		number: number,
+	});
 
-    person.save().then(result => {
-        console.log(`Added ${name} number ${number} to phonebook`)
-        mongoose.connection.close();
-    })
+	person.save().then(() => {
+		console.log(`Added ${name} number ${number} to phonebook`);
+		mongoose.connection.close();
+	});
 }
 else{
-    console.log('Phonebook: ');
+	console.log('Phonebook: ');
 
-    Phonebook
-        .find({})
-        .then(persons => {
-            persons.forEach(person => {
-                console.log(`${person.name} ${person.number}`);
-            })
-            mongoose.connection.close()
-        })
+	Phonebook
+		.find({})
+		.then(persons => {
+			persons.forEach(person => {
+				console.log(`${person.name} ${person.number}`);
+			});
+			mongoose.connection.close();
+		});
 }
