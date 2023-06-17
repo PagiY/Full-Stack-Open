7241781212b114f5789a46741a99c1ee5eb6d01a
PagiY <pagiyap003@gmail.com>
2023-06-17 21:27:10 +0800

    Removed comments for index.js; updated bloglist packages

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -7,45 +7,6 @@ app.listen(config.PORT, () => {
 	logger.info(`Serving at PORT ${config.PORT}`);
 });
 
-// const express = require('express');
-// const cors = require('cors');
-// const app = express();
-// const mongoose = require('mongoose');
 
-// const blogSchema = new mongoose.Schema({
-// 	title: String, 
-// 	author: String,
-// 	url: String,
-// 	likes: Number,
-// });	
-
-// const Blog = mongoose.model('Blog', blogSchema);
-
-// const url = 'mongodb+srv://pagiy:admin12345@bloglist.hk9gpur.mongodb.net/?retryWrites=true&w=majority';
-
-// mongoose.connect(url)
-// 	.then(() => {
-// 		console.log('successfully connected to database!');
-// 	})
-// 	.catch(() => {
-// 		console.log('error connecting to database');
-// 	});
-//middlewares
-// app.use(cors());
-// app.use(express.json());
-
-//routes
-// app.get('/api/blogs', (request, response) => {
-// 	Blog.find({})
-// 		.then(blogs => response.json(blogs));
-// });
-
-// app.post('/api/blogs', (request, response) => {
-// 	const blog = new Blog(request.body());
-
-// 	blog
-// 		.save()
-// 		.then(newBlog => response.status(201).json(newBlog));
-// });
 
 
